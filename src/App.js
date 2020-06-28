e7fdd3f2ffdc26ae19069d8bb239462bbc4f7812
Your Name <you@example.com>
2020-06-28 19:08:51 +0800

    'feat:setState初步理解'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+    state ={
+        count:0
+    }
+
+    add=()=>{
+        const {count} = this.state
+        this.setState({count:count+1},()=>{console.log('setState所指定的回调执行了')})
+    }
+
     render() {
+        console.log('render执行了')
         return (
             <div>
-                
+                <h1>当前求和为:{this.state.count}</h1>
+               <button onClick={this.add}>加一</button>
             </div>
         )
     }
